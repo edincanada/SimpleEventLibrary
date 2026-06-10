d562f6320eb6738ea4b8b9df27417aa28a4a7661
Ed Arvelaez <3144634+edincanada@users.noreply.github.com>
2026-06-09 22:06:41 -0600

    Added copyright notice

--- include/Event.t.h
+++ include/Event.t.h
@@ -1,4 +1,15 @@
-
+/*
+ * Copyright (C) 2026 Ed Arvelaez
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
 #pragma once
 #ifndef SEL_EVENT_T_H_INCLUDED
 #define SEL_EVENT_T_H_INCLUDED
